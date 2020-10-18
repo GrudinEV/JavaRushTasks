e174bab25f7d7c5b575c219d51cdf1d89a6c440f
Evgeniy <GrudinEV@gmail.com>
2020-10-18 21:59:00 +0500

    Resolve at home task about ClassLoader

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task35.task3507;
 
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,207 +9,71 @@ import java.util.Set;
 ClassLoader - что это такое?
 */
 public class Solution {
-    public static void main(String[] args) {
-        Set<? extends Animal> allAnimals = getAllAnimals(/*Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath()*/"/E:/Программирование/JavaRushTasks/out/production/4.JavaCollections/" + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
         System.out.println(allAnimals);
     }
 
-    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        Path path = Path.of(pathToAnimals.substring(1));
-        File[] arrayFiles = path.toFile().listFiles();
+    public static Set<? extends Animal> getAllAnimals(String pathToAnimals) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        File dir = new File(pathToAnimals);
+        File[] files = dir.listFiles();
         Set<Animal> set = new HashSet<>();
-        for (File file : arrayFiles) {
-            MyClassloader mcl = new MyClassloader(file.toString());
-            Class<? extends Animal> clazz = null;
-            try {
-                clazz = (Class<? extends Animal>) Class.forName("com.javarush.task.task35.task3507.data.Cat", true, mcl);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+        for (File file : files) {
+            MyClassLoader mcl = new MyClassLoader(file.getAbsolutePath());
+            Class<?> clazz = Class.forName(Solution.class.getPackage().getName() + ".data." + file.getName().replaceAll("\\.class", ""), true, mcl);
+            if (clazz instanceof Class) {
+                set.add((Animal) clazz.getDeclaredConstructor().newInstance());
             }
-            try {
-                set.add(clazz.getDeclaredConstructor().newInstance());
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-//            try {
-//                Class<? extends Animal> clazz = (Class<? extends Animal>) Class.forName(file.getName().replaceFirst(".class", ""));
-//                Animal animal = clazz.getDeclaredConstructor().newInstance();
-//                System.out.println(animal);
-//                set.add(clazz.getDeclaredConstructor().newInstance());
-//            } catch (ClassNotFoundException e) {
-//                System.out.println(e);
-//            } catch (InstantiationException e) {
-//                e.printStackTrace();
-//            } catch (InvocationTargetException e) {
-//                e.printStackTrace();
-//            } catch (NoSuchMethodException e) {
-//                e.printStackTrace();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            }
-
-//            System.out.println(file.getName().replaceFirst(".class", ""));
-//            try (FileInputStream fis = new FileInputStream(file);
-//                    DataInputStream ois = new DataInputStream(fis)) {
-//                Object o = ois.
-//                Class<? extends Animal> clazz = (Class<? extends Animal>) o.getClass();
-//                Animal animal = clazz.getDeclaredConstructor().newInstance();
-//                set.add(animal);
-//            } catch (IOException /*| ClassNotFoundException*/ e) {
-//                e.printStackTrace();
-//            } /*catch (InstantiationException e) {
-//                e.printStackTrace();
-//            } catch (InvocationTargetException e) {
-//                e.printStackTrace();
-//            } catch (NoSuchMethodException e) {
-//                e.printStackTrace();
-//            } catch (IllegalAccessException e) {
-//                e.printStackTrace();
-//            }*/
         }
         return set;
     }
 
-    static class MyClassloader extends ClassLoader
-    {
-        public final String classPath;
+    static class MyClassLoader extends ClassLoader {
+        final String classPath;
 
-        public MyClassloader(String classPath)
-        {
-            this.classPath= classPath;
+        MyClassLoader(String classPath) {
+            this.classPath = classPath;
         }
 
         @Override
-        protected synchronized Class loadClass(String name, boolean resolve) throws ClassNotFoundException
-        {
-            File f = new File(classPath);
-            System.out.println(name);
-            byte[] classBytes= new byte[0];
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            InputStream is = null;
             try {
-                classBytes = loadFileAsBytes(f);
+                is = new FileInputStream(new File(classPath));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            long length = new File(classPath).length();
+            byte[] bytes = new byte[(int)length];
+
+            int offset = 0;
+            int numRead = 0;
+            while (true) {
+                try {
+                    if (!(offset < bytes.length
+                                        && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                offset += numRead;
+            }
+
+            if (offset < bytes.length) {
+                try {
+                    throw new IOException("Could not completely read file " + classPath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                is.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Class result = defineClass(name, classBytes, 0, classBytes.length);
-            return result;
-        }
 
-//        protected Class findClass(String name) throws ClassNotFoundException
-//        {
-//            Class result= (Class)classesHash.get(name);
-//            if (result!=null) {
-//                /*
-//                 * System.out.println("% Class " + name +
-//                 *                       " found in cache");
-//                 */
-//                return result;
-//            }
-//
-//            File f= findFile(name.replace('.','/'),".class");
-//            // Класс mypackage.MyClass следует искать файле
-//            // mypackage/MyClass.class
-//            /*
-//             * System.out.println("% Class " + name +
-//             *                    (f==null?"":" found in "+f));
-//             */
-//            if (f==null) {
-//                // Обращаемся к системному загрузчику в случае
-//                // неудачи. findSystemClass – это метод
-//                // абстрактного класса ClassLoader с объявлением
-//                // protected Class findSystemClass(String name)
-//                // (т.е. предназначенный для использования в
-//                // наследниках и не подлежащий переопределению).
-//                // Он выполняет поиск и загрузку класса по
-//                // алгоритму системного загрузчика. Без вызова
-//                // findSystemClass(name) нам пришлось бы
-//                // самостоятельно позаботиться о загрузке всех
-//                // стандартных библиотечных классов типа
-//                // java.lang.String, что потребовало бы
-//                // реализовать работу с JAR-архивами
-//                // (стандартные библиотеки почти всегда
-//                // упакованы в JAR)
-//                return findSystemClass(name);
-//            }
-//
-//            try {
-//                byte[] classBytes= loadFileAsBytes(f);
-//                result= defineClass(name, classBytes, 0,
-//                        classBytes.length);
-//            } catch (IOException e) {
-//                throw new ClassNotFoundException(
-//                        "Cannot load class " + name + ": " + e);
-//            } catch (ClassFormatError e) {
-//                throw new ClassNotFoundException(
-//                        "Format of class file incorrect for class "
-//                                + name + " : " + e);
-//            }
-//            classesHash.put(name,result);
-//            return result;
-//        }
-//        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//        protected java.net.URL findResource(String name)
-//        {
-//            File f= findFile(name, "");
-//            if (f==null)
-//                return null;
-//            try {
-//                return f.toURL();
-//            } catch(java.net.MalformedURLException e) {
-//                return null;
-//            }
-//        }
-//        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//        /**
-//         * Поиск файла с именем name и, возможно, расширением
-//         * extension в каталогах поиска, заданных параметром
-//         * конструктора classPath. Имена подкаталогов в name
-//         * разделяются символом '/' – даже если в операционной
-//         * системе принят другой разделитель для подкаталогов.
-//         * (Именно в таком виде получает свой параметр метод
-//         * findResource.)
-//         */
-//        private File findFile(String name, String extension)
-//        {
-//            File fl
-//            for (int k=0; k <classPath.length; k++) {
-//                f = new File((new File(classPath[k])).getPath()
-//                        + File.separatorChar
-//                        + name.replace('/',
-//                        File.separatorChar)
-//                        + extension);
-//                if (f.exists())
-//                    return f;
-//            }
-//            return null;
-//        }
-//        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        public static byte[] loadFileAsBytes(File file)
-                throws IOException
-        {
-            byte[] result = new byte[(int)file.length()];
-            FileInputStream f = new FileInputStream(file);
-            try {
-                f.read(result,0,result.length);
-            } finally {
-                try {
-                    f.close();
-                } catch (Exception e) {
-                    // Игнорируем исключения, возникшие при
-                    // вызове close. Они крайне маловероятны и не
-                    // очень важны - файл уже прочитан. Но если
-                    // они все же возникнут, то они не должны
-                    // замаскировать действительно важные ошибки,
-                    // возникшие при вызове read.
-                };
-            }
-            return result;
+            return defineClass(name, bytes, 0, bytes.length);
         }
-//        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     }
 }
