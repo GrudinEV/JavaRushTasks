eabffe19f5967b37bf1f12c29a276691c2768aa9
Evgeniy <grudinev@gmail.com>
2020-10-22 11:32:48 +0500

    Resolved task 3507

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task35.task3507;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,7 +11,7 @@ ClassLoader - что это такое?
 */
 public class Solution {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        Set<? extends Animal> allAnimals = getAllAnimals(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
+        Set<? extends Animal> allAnimals = getAllAnimals(/*"E:\\Программирование\\JavaRushTasks\\out\\production\\4.JavaCollections\\"*/Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + Solution.class.getPackage().getName().replaceAll("[.]", "/") + "/data");
         System.out.println(allAnimals);
     }
 
@@ -21,8 +22,16 @@ public class Solution {
         for (File file : files) {
             MyClassLoader mcl = new MyClassLoader(file.getAbsolutePath());
             Class<?> clazz = Class.forName(Solution.class.getPackage().getName() + ".data." + file.getName().replaceAll("\\.class", ""), true, mcl);
-            if (clazz instanceof Class) {
-                set.add((Animal) clazz.getDeclaredConstructor().newInstance());
+            Constructor<?>[] constructors = clazz.getConstructors();
+            if (constructors.length > 0) {
+                for (Constructor<?> constructor : constructors) {
+                    if (constructor.getParameterCount() == 0) {
+                        Animal animal = (Animal) clazz.getDeclaredConstructor().newInstance();
+                        if (animal instanceof Animal) {
+                            set.add((Animal) clazz.getDeclaredConstructor().newInstance());
+                        }
+                    }
+                }
             }
         }
         return set;
@@ -36,43 +45,21 @@ public class Solution {
         }
 
         @Override
-        protected Class<?> findClass(String name) throws ClassNotFoundException {
-            InputStream is = null;
-            try {
-                is = new FileInputStream(new File(classPath));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-
+        protected Class<?> findClass(String name){
             long length = new File(classPath).length();
             byte[] bytes = new byte[(int)length];
-
             int offset = 0;
             int numRead = 0;
-            while (true) {
-                try {
-                    if (!(offset < bytes.length
-                                        && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0)) break;
-                } catch (IOException e) {
-                    e.printStackTrace();
+            try (InputStream is = new FileInputStream(new File(classPath))) {
+                while (true) {
+                        if (!(offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0)) {
+                            break;
+                        }
+                    offset += numRead;
                 }
-                offset += numRead;
-            }
-
-            if (offset < bytes.length) {
-                try {
-                    throw new IOException("Could not completely read file " + classPath);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            try {
-                is.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return defineClass(name, bytes, 0, bytes.length);
         }
     }
